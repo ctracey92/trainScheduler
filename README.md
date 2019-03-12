1a12911a59cb9dcae9d62e78f3f1f18705d7c5d6
Cody <cody.tracy92@gmail.com>
2019-03-12 15:01:04 -0500

    added in comments for readability, and styled the read me

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # trainScheduler
+Student Contributors
+@ctracey92
+
+* Technology
+ CSS3, HTML5, Javascript, jQuery, Firebase
+
+* About
+A train schedulng app. The user inputs the train name, destination, firts arrival time, and how often it runs. Based on the user input the application will calculate the number of minutes until the next train arrival, and what time it will arrive at. The input, and calculated variables will be stored in firebase so that it will be loaded on every use of the page. 
+
+
+* Contributing Guidelines
+All contributions and suggestions are welcome! For direct contributions, please fork the repository and file a pull request.
+
+## Contact \(soon to be\) Developer/Full-stack Web Software Developer
+* Homepage: https://github.com/ctracey92/
+* e-mail: cody.tracey92@gmail.com
+* LinkedIn: https://www.linkedin.com/in/cody-tracey-353432179
--- logic.js
+++ logic.js
@@ -9,39 +9,51 @@ var config = {
 };
 firebase.initializeApp(config);
   
+  //Global Variables Declared
   var database = firebase.database();
   var trainName;
   var destination;
   var firstTrain;
   var frequency;
   
+    //When a child is added run the following funciton
     database.ref().on("child_added", function(snapshot){
       
+      //Console log the snapshot
       console.log(snapshot);
+
+      //Assign the snapshot pieces to variables
       trainName = snapshot.val().trainName;
       destination = snapshot.val().destination;
       firstTrainTime = snapshot.val().firstTrainTime;
       frequency = snapshot.val().frequency;
   
+      //Create the date variables
       var randomFormat = "MM/DD/YYYY";
       var convertedDate = moment(firstTrainTime, randomFormat);
   
+      //Console log the date variable
       console.log(convertedDate.toNow());
 
+      //Convert the time variable
       var firstTimeConverted = moment(firstTrainTime, "HH:mm").subtract(1, "years");
       
       var currentTime = moment();
       console.log(currentTime);
       
+  
+      //Calculate the time difference  
       var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
       
       var tRemainder = diffTime % frequency;
 
       var tMinutesTillTrain = frequency - tRemainder;
 
+      //Define the next train variable and convert it
       var nextTrain = moment().add(tMinutesTillTrain, "minutes");
       var nextTrain = (moment(nextTrain).format("HH:mm"));
 
+      //Add rows with the variable data for each of the child nodes
       function addRow(){
           var rows =  $("<tr>");
            
@@ -52,6 +64,7 @@ firebase.initializeApp(config);
           var minutesTillTrainTD = $("<td>").attr("scope", "col").text(tMinutesTillTrain);
           (rows).append(trainNameTD, destinationTD, frequencyTD, nextTrainTD, minutesTillTrainTD);
         
+          //Add the rows onto the row
         $("#trains").append(rows);
       }
   
@@ -66,12 +79,15 @@ firebase.initializeApp(config);
       console.log ("error")
     }
   
+    //On click of the submit button run the following function
 $("#submitBttn").on("click", function(){
+    //Capture the values from the form and assign them to a variable
     var trainName = $("#trainName").val().trim();
     var destination = $("#destination").val().trim();
     var firstTrainTime = $("#firstTrainTime").val().trim();
     var frequency= $("#frequency").val().trim();
-    
+
+  //Push the child nodes to firebase
 database.ref().push({
   trainName: trainName,
   destination: destination,
